be47b54b45ea540a2446be97f8dd3914a6d5e891
iregon <0iregon0@gmail.com>
2018-02-24 13:04:21 +0100

    sum

--- query.sql
+++ query.sql
@@ -2,3 +2,11 @@ SELECT AVG(v.Voto), s.Nome, s.Cognome
 FROM valutazioni v, studenti s
 WHERE s.Matricola = 14
 AND s.Matricola = v.Matricola
+
+/*ore di insegnamento del docente massimo palombo*/
+
+SELECT SUM(i.NumeroOre) as "totaleore", p.Nome, p.Cognome
+FROM insegnano i, professori p
+WHERE p.Nome = "Massimo"
+	AND p.Cognome = "Palombo"
+    AND p.CodiceProfessore = i.CodiceProfessore
